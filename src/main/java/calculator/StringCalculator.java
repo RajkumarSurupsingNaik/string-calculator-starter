0e5624f9650c16bd8eb85a191896470909b84f6f
Rajkumar Naik <naikrajkumar2021@gmail.com>
2021-10-17 09:43:47 +0530

    empty string should return 0

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,13 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	if(input.isEmpty())	
+    		return 0;
+    	else if(input.contains(",")) {
+    		String [] tk= input.split(",");
+    		return Integer.parseInt(tk[0])+ Integer.parseInt(tk[1]);
+    	}else
+    		return Integer.parseInt(input);
     }
 
 }
